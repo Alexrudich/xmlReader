f384df28706ef81ef51dd716c542cbfbff3cbe31
MINSK\a.rudich <rudichalex@yandex.ru>
2018-05-21 10:49:50 +0300

    simple xml reader

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace XmlReader
 {
@@ -10,6 +11,18 @@ namespace XmlReader
     {
         static void Main(string[] args)
         {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(@"C:\Users\a.rudich\Documents\Projects-work\Metanit\XmlReader\products.xml");
+            XmlNodeList nodeList = xmlDoc.DocumentElement.SelectNodes("/Table/Product");
+            string proID = "", proName = "", price = "";
+            foreach (XmlNode node in nodeList)
+            {
+                proID = node.SelectSingleNode("Product_id").InnerText;
+                proName = node.SelectSingleNode("Product_name").InnerText;
+                price = node.SelectSingleNode("Product_price").InnerText;
+                Console.WriteLine(proID + " " + proName + " " + price);
+            }
+            Console.ReadLine();
         }
     }
 }
