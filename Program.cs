eafdb2b1e5e6e47e9b4fb864fdcc973514c424cd
MINSK\a.rudich <rudichalex@yandex.ru>
2018-05-23 11:12:40 +0300

    Recognize SMGS node

--- Program.cs
+++ Program.cs
@@ -33,40 +33,54 @@ namespace SimleXmlReader
         {
             string RootFolder = ConfigurationManager.AppSettings["RootFolder"];
             DirectoryInfo di = new DirectoryInfo(RootFolder);
-            FileInfo[] files = di.GetFiles("*.xml"); // Read files xml from folder
-            var temp = files.Length;
+            FileInfo[] files = di.GetFiles("*.xml"); // Read xml files from folder
             foreach (FileInfo file in files)
             {
                 if (file != null)
                 {
-                    XDocument Xdoc = XDocument.Load(file.FullName);
-                    var tempObj = Xdoc.ToString();   //xml в стрингу
+                    XDocument xdoc = XDocument.Load(file.FullName);
+                    var tempObj = xdoc.ToString();   //xml to string
                     XDocument doc = XDocument.Parse(tempObj);
-                    XElement result = doc.XPathSelectElement("descendant::G01_N[G01]");
-
                     List<ParsedInfo> NodeInfo = new List<ParsedInfo>();
-                    if (result != null)
+                   
+                    XElement TrNumber = doc.XPathSelectElement("descendant::G01_N[G01]"); // Path to parent node + [child node]
+                    if (TrNumber != null)
                     {
-                        IEnumerable<XElement> keyWords = result.Elements("G01");
+                        IEnumerable<XElement> keyWords = TrNumber.Elements("G01");
                         NodeInfo = (from itm in keyWords
                                     where itm.Element("TR_NOMER") != null
-                                        //&& itm.Element("category").Value == "verb"
-                                        //&& itm.Element("id") != null
-                                        //&& itm.Element("base") != null
+                                       
                                     select new ParsedInfo()
                                     {
                                         TransportNumber = itm.Element("TR_NOMER").Value,
-                                        //Category = itm.Element("category").Value,
-                                        //Id = itm.Element("id").Value,
                                     }).ToList();
                     }
+                    XElement SMGS = doc.XPathSelectElement("descendant::Declarant[G02]");
+                    List<ParsedInfo> SMGList = new List<ParsedInfo>();
+                    if (SMGS != null)
+                    {
+                        IEnumerable<XElement> keyWords1 = SMGS.Elements("G02");
+                        SMGList = (from itm in keyWords1
+                                    where itm.Element("KOD_DOC") != null && itm.Element("KOD_DOC").Value == "02013"
+
+
+                                   select new ParsedInfo()
+                                    {
+                                        SMGSNumber = itm.Element("NOM_DOC").Value,
+                                        SMGSDate = itm.Element("DATE_DOC").Value,
+                                    }).ToList();
+                    }
+                   
                     else
                     {
                         Console.WriteLine("Код по прежнему говно. Попробуй еще раз!");
                     }
+                    NodeInfo.AddRange(SMGList);
                     foreach (ParsedInfo p in NodeInfo)
                     {
-                        Console.WriteLine(p.TransportNumber);
+                        Console.WriteLine("TransportNumber - {0}", p.TransportNumber);
+                        Console.WriteLine("SMGSNumber - {0}", p.SMGSNumber);
+                        Console.WriteLine("SMGSDate - {0}", p.SMGSDate);
                     }
                     Console.ReadLine();
                 } 
